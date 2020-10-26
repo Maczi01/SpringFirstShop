d671d1a1144ae2b7cc27824e3218427f36e3115d
Maczi01 <mateusz.w.twardy@gmail.com>
2020-10-26 18:21:32 +0100

    remove additional list

--- src/main/java/pl/maczi/springboottest/shop/ProductService.java
+++ src/main/java/pl/maczi/springboottest/shop/ProductService.java
@@ -12,7 +12,7 @@ public class ProductService {
     private double tax;
     @Value("${shop.discount}")
     private double discount;
-
+    private final double SUM_QUALIFYING_TO_DISCOUNT = 500;
 
     public ProductService() {
 
@@ -20,21 +20,14 @@ public class ProductService {
 
     public double getFiveRandomProducts() {
         List<Product> productList = new ArrayList<>();
-        for (int i = 0; i <= 5; i++) {
+        for (int i = 0; i < 5; i++) {
             productList.add(new Product());
         }
 
         List<Integer> toSum = new ArrayList<>();
 
-        for (Product product : productList) {
-            toSum.add((int) (product.getPrice() + (product.getPrice() * tax)));
-        }
-
-
-
-        
-        double sum = toSum.stream().mapToInt(a -> a).sum();
-        if (sum > 500) {
+        double sum = productList.stream().map(e -> e.getPrice()).mapToDouble(a -> a).sum();
+        if (sum > SUM_QUALIFYING_TO_DISCOUNT) {
             sum = sum - (sum * discount);
         }
         return sum;
